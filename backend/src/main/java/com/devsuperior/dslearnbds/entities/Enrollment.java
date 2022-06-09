0e964c2d0a86da8ddac77e5bf37053028fe3d82b
Roberto Okamura <rob.okamura@gmail.com>
2022-06-08 23:39:03 -0300

    Lesson, Content and Task

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Enrollment.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Enrollment.java
@@ -1,17 +1,22 @@
 package com.devsuperior.dslearnbds.entities;
 
+import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 import com.devsuperior.dslearnbds.entities.pk.EnrollmentPK;
 
 @Entity
 @Table(name = "tb_enrollment")
-public class Enrollment {
+public class Enrollment implements Serializable{
+	private static final long serialVersionUID = 1L;
 	
 	//chave composta DEVE ser instanciado
 	@EmbeddedId
@@ -24,6 +29,9 @@ public class Enrollment {
 	private Instant refundMoment;
 	private boolean available;
 	private boolean onlyUpdate;
+	
+	@ManyToMany(mappedBy = "enrollmentsDone")
+	private Set<Lesson> lessonsDone = new HashSet<>();
 	
 	public Enrollment() {
 	}
@@ -85,9 +93,9 @@ public class Enrollment {
 	public void setOnlyUpdate(boolean onlyUpdate) {
 		this.onlyUpdate = onlyUpdate;
 	}
-	
-	
-	
-	
 
+	public Set<Lesson> getLessonsDone() {
+		return lessonsDone;
+	}
+	
 }

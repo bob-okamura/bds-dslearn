93dd882f47d37bdf6b994fd9579fec9a6afa0e72
Roberto Okamura <rob.okamura@gmail.com>
2022-06-09 01:14:05 -0300

    Deliver, DeliveryStatus and Deliver seed

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -47,7 +47,7 @@ INSERT INTO tb_task (id, description, question_Count, Approval_Count, weight, du
 INSERT INTO tb_lessons_done (lesson_id, user_id, offer_id) VALUES(1, 1, 1);
 INSERT INTO tb_lessons_done (lesson_id, user_id, offer_id) VALUES(2, 1, 1);
 
-
+INSERT INTO tb_deliver (uri, moment, status, feedback, correct_Count, lesson_id, user_id, offer_id) VALUES ('https://github.com/bob-okamura/bds-dslearn/commits/main', TIMESTAMP WITH TIME ZONE '2020-07-13T13:00:00Z', 0, null, null, 4, 1, 1);
 
 
 
